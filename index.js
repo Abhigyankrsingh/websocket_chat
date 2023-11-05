d5a92bcf67ebcb2ce0d14c8d51fef0087b5f649b
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2023-11-06 00:37:04 +0530

    Bug fixed

--- index.js
+++ index.js
@@ -8,4 +8,8 @@ const server = http.createServer(app);
 
 app.use(express.static("/public"));
 
+app.get('/',(req,res)=>{
+ return res.sendFile("/index.html")
+});
+
 server.listen(9000, () =>console.log(`Server Started at:9000`))
