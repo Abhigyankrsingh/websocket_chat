536a1154b11aecbb1e46bf1c42ec812158ebc653
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2023-11-05 23:54:12 +0530

    Chat_App

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require("http");
+
+const express = require("express");
+
+const app = express();
+
+const server = http.createServer(app);
+
+app.use(express.static("/public"));
+
+server.listen(9000, () =>console.log(`Server Started at:9000`))
