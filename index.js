3c306f30ea51e21dc17979bee30d4b21ea9d639e
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2023-11-06 00:37:57 +0530

    Error Fixed

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ const http = require("http");
 
 const express = require("express");
 
+const path = require('path');
+
+
+
 const app = express();
 
 const server = http.createServer(app);
 
-app.use(express.static("/public"));
+app.use(express.static(path.resolve("./public")));
 
 app.get('/',(req,res)=>{
- return res.sendFile("/index.html")
+ return res.sendFile("/public/index.html")
 });
 
 server.listen(9000, () =>console.log(`Server Started at:9000`))
