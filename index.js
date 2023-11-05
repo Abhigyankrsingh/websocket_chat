66af5fa2d7088c32770e413e2a6b5d4b04c63d42
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2023-11-06 01:01:13 +0530

    Code_updated

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ const express = require("express");
 
 const path = require('path');
 
+const {Server} = require("socket.io");
+
 
 
 const app = express();
 
 const server = http.createServer(app);
+
+const io = new Server(server);
+
+io.on('connection', (socket) =>{
+    console.log("A new user has connected", socket.id);
+} );
 
 app.use(express.static(path.resolve("./public")));
 
