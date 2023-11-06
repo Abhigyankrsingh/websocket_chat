ef3242e0c50b8b62cc8aa2ff18962390f769c73c
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2023-11-06 16:21:53 +0530

    Full eror_fixed

--- index.js
+++ index.js
@@ -1,31 +1,23 @@
 const http = require("http");
-
 const express = require("express");
-
 const path = require('path');
-
-const {Server} = require("socket.io");
-
-
+const { Server } = require("socket.io");
 
 const app = express();
-
 const server = http.createServer(app);
-
 const io = new Server(server);
 
-io.on('connection', (socket) =>{
-   socket.on("userr-message", (message)=>{
+io.on('connection', (socket) => {
+  socket.on("user-message", (message) => {
     console.log("A new User Message", message);
-   });
-} );
-
-app.use(express.static(path.resolve("./public")));
-
-
-
-app.get('/',(req,res)=>{
- return res.sendFile("/public/index.html")
+    io.emit("chat-message", message); // Broadcasting the message to all connected clients
+  });
 });
 
-server.listen(9000, () =>console.log(`Server Started at:9000`))
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+server.listen(9000, () => console.log('Server Started at: 9000'));
